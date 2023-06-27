c40dc96e3635d536dc93696b7886e3f8273c2e4b
Priyanshu Singh <62940545+PriyanshuSingh8750@users.noreply.github.com>
2023-06-28 00:19:32 +0530

    Create 645. Set Mismatch.cpp

---
+++ 645. Set Mismatch.cpp
@@ -0,0 +1,15 @@
+     sort(nums.begin(),nums.end());
+        int a=0,b=0;
+        for(int i=1;i<nums.size();i++ ){
+            if(nums[i]==nums[i-1]){
+                a=nums[i];
+                break;
+            }
+        }
+        for(int i=1;i<=nums.size();i++){
+            if(!binary_search(nums.begin(),nums.end(),i)){
+                b=i;
+                break;
+            }
+        }
+        return {a,b};
